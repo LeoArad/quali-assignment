89c5d116ed06d7c3b2ae242c90d1225bf7e4bdb0
LeoArad <liroy091@gmail.com>
2025-09-05 12:46:17 -0500

    Initial clean commit

--- quali-assignmen/outputs.tf
+++ quali-assignmen/outputs.tf
@@ -1,8 +1,8 @@
 output "bucket_name" {
   description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.leeron_bucket.id
+  value       = aws_s3_bucket.leeron-s3.id
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.leeron_bucket.arn
+  value = aws_s3_bucket.leeron-s3.arn
 }
