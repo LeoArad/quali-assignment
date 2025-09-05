06b1c8ddb9e295c1213b9a3c200c253b6df63f67
LeoArad <liroy091@gmail.com>
2025-09-05 14:03:01 -0500

    Initial clean commit

--- quali-assignmen/variables.tf
+++ quali-assignmen/variables.tf
@@ -15,14 +15,3 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
